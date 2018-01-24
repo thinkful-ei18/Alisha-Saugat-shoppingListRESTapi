23692c1b446eebcaa2018102e44f0b3fc6774178
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-24 16:35:59 -0500

    delete item refactored

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -93,9 +93,13 @@ const shoppingList = (function () {
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      store.findAndDelete(id);
+      api.deleteItem(id, () => {
+        store.findAndDelete(id);
+        render();
+      });
+      // store.findAndDelete(id);
       // render the updated shopping list
-      render();
+      // render();
     });
   }
 
