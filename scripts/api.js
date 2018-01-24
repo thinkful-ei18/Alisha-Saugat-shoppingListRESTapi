7e43b231e2d37869aa4beb61a4a017de673bddcb
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-24 12:18:59 -0500

    createItem added

--- scripts/api.js
+++ scripts/api.js
@@ -4,10 +4,33 @@ const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/saugat';
 
   const getItems = function (callback) {
+    $.getJSON(`${BASE_URL}/items`, callback);
+
+  };
+
+  const createItem = function (name, callback) {
+    const newItem = {
+      name
+    };
+
+
+    const item = JSON.stringify(newItem);
+
+
+    const callSettings = {
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: item,
+      success: callback
+    };
+
+    $.ajax(callSettings);
 
   };
 
   return {
-    getItems
+    getItems,
+    createItem
   };
 }());
